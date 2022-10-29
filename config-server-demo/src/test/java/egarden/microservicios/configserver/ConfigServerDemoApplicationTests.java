e3a140ea140be734804e11b8f35f6324efcc4801
joffre <joffre.hermosilla@gmail.com>
2022-10-29 16:23:44 -0500

    prueba de config server

---
+++ config-server-demo/src/test/java/egarden/microservicios/configserver/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package egarden.microservicios.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
